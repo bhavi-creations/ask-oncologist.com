20035fd22c7854608a27b5a347cbbde2c4209659
manimalladi <manimalladi05@gmail.com>
2025-12-23 09:22:13 +0530

    to change the styling for fullblogs

--- admin/public/allBlog.php
+++ admin/public/allBlog.php
@@ -63,7 +63,7 @@
                     <body>
 
 
-                    <h1>blogs for ask oncologist</h1>
+                   
                         <div class="container">
                             <div class="d-sm-flex align-items-center justify-content-between mb-4">
                                 <h2 class="h2 mb-0 text-info mx-2"> Published Blogs</h2>
